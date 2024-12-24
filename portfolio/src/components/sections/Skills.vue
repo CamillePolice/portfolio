f9cf4c0062c08fbd8632a2f7baa3e1f439c81015
camillepolice@icloud.com <Ap4hgfpaz.>
2024-12-24 18:21:31 +0100

    feat: enhance skills part

--- portfolio/src/components/sections/Skills.vue
+++ portfolio/src/components/sections/Skills.vue
@@ -8,7 +8,7 @@ defineProps({
 
 const languages = ['C', 'C++', 'Java', 'TypeScript/JavaScript', 'GO', 'Python', 'PHP']
 const frameworks = ['Angular', 'Symfony', 'Gin', 'Drupal', 'Vue3 (Nuxt)']
-const otherSkills = ['Tech Lead', 'Project Management', 'Communication', 'CI/CD', 'Microservices']
+const otherSkills = ['Tech Lead', 'Project Management', 'Communication', 'CI/CD', 'Microservices', 'Building Devcontainer', 'Pro VSCode']
 </script>
 
 <template>
@@ -18,8 +18,8 @@ const otherSkills = ['Tech Lead', 'Project Management', 'Communication', 'CI/CD'
       <div class="max-w-4xl mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div class="space-y-6">
-            <h3 class="text-xl font-semibold mb-4">Languages</h3>
-            <div class="flex flex-wrap gap-2">
+            <h3 class="text-xl font-semibold mb-4 text-center">Languages</h3>
+            <div class="flex flex-wrap gap-2 justify-center">
               <span v-for="lang in languages" :key="lang"
                 class="px-3 py-1 bg-purple-100 text-purple-700 rounded-full">
                 {{ lang }}
@@ -28,25 +28,24 @@ const otherSkills = ['Tech Lead', 'Project Management', 'Communication', 'CI/CD'
           </div>
 
           <div class="space-y-6">
-            <h3 class="text-xl font-semibold mb-4">Frameworks</h3>
-            <div class="flex flex-wrap gap-2">
+            <h3 class="text-xl font-semibold mb-4 text-center">Frameworks</h3>
+            <div class="flex flex-wrap gap-2 justify-center">
               <span v-for="framework in frameworks" :key="framework"
                 class="px-3 py-1 bg-blue-100 text-blue-700 rounded-full">
                 {{ framework }}
               </span>
             </div>
           </div>
-
-          <div class="space-y-6">
-            <h3 class="text-xl font-semibold mb-4">Other Skills</h3>
-            <div class="flex flex-wrap gap-2">
+        </div>
+        <div class="max-w-lg mx-auto mt-8 space-y-6">
+            <h3 class="text-xl font-semibold mb-4 text-center">Other Skills</h3>
+            <div class="flex flex-wrap gap-2 justify-center">
               <span v-for="skill in otherSkills" :key="skill"
                 class="px-3 py-1 bg-green-100 text-green-700 rounded-full">
                 {{ skill }}
               </span>
             </div>
           </div>
-        </div>
       </div>
     </div>
   </section>
